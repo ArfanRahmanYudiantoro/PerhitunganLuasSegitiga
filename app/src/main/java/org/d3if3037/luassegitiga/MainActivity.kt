2cb4a8fd69237e6a4f2811a18ed4247e3d75c217
ArfanRahmanYudiantoro <arfanrahman@student.telkomuniversity.ac.id>
2023-03-30 22:58:17 +0700

    2.pembuatan code rumus

--- app/src/main/java/org/d3if3037/luassegitiga/MainActivity.kt
+++ app/src/main/java/org/d3if3037/luassegitiga/MainActivity.kt
@@ -2,12 +2,21 @@ package org.d3if3037.luassegitiga
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import org.d3if3037.luassegitiga.databinding.ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private fun luasSegitiga() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
+  binding= ActivityMainBinding.inflate(layoutInflater)
+setContentView(binding.root)
+binding.button.setOnClickListener { luasSegitiga()}
+}
+private fun luasSegitiga(){
+    val alas = binding.editTextNumber.text.toString().toInt()
+    val tinggi = binding.editTextNumber2.text.toString().toInt()
+    val hasil = (alas * tinggi) / 2
+
+}
 }
