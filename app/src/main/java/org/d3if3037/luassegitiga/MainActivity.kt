0932aa16e15715e73e7cc2fcaa6afae9cfca74a2
ArfanRahmanYudiantoro <arfanrahman@student.telkomuniversity.ac.id>
2023-03-30 21:08:32 +0700

    1.pembutan tampilan

--- app/src/main/java/org/d3if3037/luassegitiga/MainActivity.kt
+++ app/src/main/java/org/d3if3037/luassegitiga/MainActivity.kt
@@ -2,10 +2,12 @@ package org.d3if3037.luassegitiga
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.d3if3037.luassegitiga.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private fun luasSegitiga() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
