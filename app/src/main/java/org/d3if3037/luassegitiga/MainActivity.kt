fcda051d8729407c0901d9376ec8de506fa06318
ArfanRahmanYudiantoro <arfanrahman@student.telkomuniversity.ac.id>
2023-03-30 19:49:46 +0700

    1.pembutan tampilan

---
+++ app/src/main/java/org/d3if3037/luassegitiga/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.d3if3037.luassegitiga
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
