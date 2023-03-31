f019783614aeeae7b6cd6390e2c24704c57a9744
ArfanRahmanYudiantoro <arfanrahman@student.telkomuniversity.ac.id>
2023-03-31 16:38:40 +0700

    3.debugging dan perbaikan

--- app/src/main/java/org/d3if3037/luassegitiga/MainActivity.kt
+++ app/src/main/java/org/d3if3037/luassegitiga/MainActivity.kt
@@ -2,21 +2,32 @@ package org.d3if3037.luassegitiga
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Toast
 import org.d3if3037.luassegitiga.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-  binding= ActivityMainBinding.inflate(layoutInflater)
-setContentView(binding.root)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 binding.button.setOnClickListener { luasSegitiga()}
 }
-private fun luasSegitiga(){
-    val alas = binding.editTextNumber.text.toString().toInt()
-    val tinggi = binding.editTextNumber2.text.toString().toInt()
-    val hasil = (alas * tinggi) / 2
-
+    private fun luasSegitiga(){
+        val alas = binding.inputAlas.text.toString()
+        if (TextUtils.isEmpty( alas )) {
+            Toast.makeText(this, R.string.alas_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val tinggi = binding.inputTinggi.text.toString()
+        if (TextUtils.isEmpty( tinggi )) {
+            Toast.makeText(this, R.string.tinggi_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val hasil = (alas.toFloat() * tinggi.toFloat()) / 2
+        binding.hasilTextView.text = getString(R.string.luas_x, hasil)
 }
 }
